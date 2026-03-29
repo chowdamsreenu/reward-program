e5eeb492273ef39555675ec613b6a6f5fae17349
chowdamsreenu <82132328+chowdamsreenu@users.noreply.github.com>
2026-03-29 18:58:06 +0530

    Update RewardService.java

--- src/main/java/com/retailer/reward/RewardService.java
+++ src/main/java/com/retailer/reward/RewardService.java
@@ -21,25 +21,26 @@ public class RewardService {
         Map<Long, List<Transaction>> customerMap =
                 transactions.stream()
                         .collect(Collectors.groupingBy(Transaction::getCustomerId));
-
+// 101 → [txn1, txn2, txn5]
+//	102 → [txn3, txn4]
         List<RewardResponse> responseList = new ArrayList<>();
-
+// Loop Each Transaction
         for (Long customerId : customerMap.keySet()) {
 
             Map<String, Integer> monthlyPoints = new HashMap<>();
             int totalPoints = 0;
 
             for (Transaction txn : customerMap.get(customerId)) {
-
+//Calculate Points
                 int points = RewardUtil.calculatePoints(txn.getAmount());
-
+//Extract Month
                 String month = txn.getDate()
                         .getMonth()
                         .getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
-
+ // Store Monthly Points
                 monthlyPoints.put(month,
                         monthlyPoints.getOrDefault(month, 0) + points);
-
+ // Add Total
                 totalPoints += points;
             }
 
